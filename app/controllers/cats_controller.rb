969395a2a6f40bef6bfbfd6b6106f8291e1d14c8
Kuroneko <ken.aster8@gmail.com>
2023-10-06 11:20:52 +0900

    FlashのTurbo Streams化

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -43,6 +43,7 @@ class CatsController < ApplicationController
   def update
     if @cat.update(cat_params)
       # redirect_to @cat, notice: "ねこを更新しました。", status: :see_other
+      flash.now.notice = "ねこを更新しました。"
     else
       render :edit, status: :unprocessable_entity
     end
